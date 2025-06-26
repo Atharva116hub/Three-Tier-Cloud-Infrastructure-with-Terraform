d16b81e6e1bfc66cafac352159588bc059d6f061
Atharva116hub <atharva.topre116@gmail.com>
2025-06-26 16:36:22 +0530

    some file added

--- output.tf
+++ output.tf
@@ -1,19 +1,19 @@
 output "vpc_id" {
-    value = module.vpc.vpc_id
+  value = module.vpc.vpc_id
 }
 
 output "ec2_instance_id" {
-    value = module.ec2.web_instance_id
+  value = module.ec2.instance_id
+}
 
+output "ec2_public_ip" {
+  value = module.ec2.public_ip
 }
 
 output "rds_endpoint" {
-    value = module.rds.db_endpoint
-  
+  value = module.rds.db_endpoint
 }
 
 output "alb_dns_name" {
-    value = module.alb.alb_dns_name
-  
+  value = module.alb.dns_name
 }
-
